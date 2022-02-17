9c55249e67c365ae73e006fd60c6f6b21ad2c971
Cristian Prodaniuc <crisniuc@gmail.com>
2022-02-17 23:59:03 +0200

    listEvents and Logout

--- main_window.py
+++ main_window.py
@@ -19,6 +19,8 @@ class Main(QMainWindow):
         self.window.buttonAPItest.clicked.connect(self.APItest)
         self.window.buttonDisplayResults.clicked.connect(self.displayResults)
         self.window.buttonLoginTest.clicked.connect(self.loginTest)
+        self.window.buttonListEvents.clicked.connect(self.listEvents)
+        self.window.buttonLogoutTest.clicked.connect(self.logoutTest)
 
     def openAdjustBookmakerOdds(self):
         self.widgetAdjustBookmakerOdds = AdjustBookmakerOdds('gui\\adjust_bookmaker_odds.ui')
@@ -29,27 +31,53 @@ class Main(QMainWindow):
 
         payload = lines[0]
         headers = {'X-Application': 'h3wqckK1N6QPkUrC', 'Content-Type': 'application/x-www-form-urlencoded'}
+        self.resp = requests.post('https://identitysso-cert.betfair.ro/api/certlogin', data=payload, cert=('E:\\Software\\openssl-3.0.1\\apps\\client-2048.crt', 'E:\\Software\\openssl-3.0.1\\apps\\client-2048.key'), headers=headers)
  
-        resp = requests.post('https://identitysso-cert.betfair.ro/api/certlogin', data=payload, cert=('E:\\Software\\openssl-3.0.1\\apps\\client-2048.crt', 'E:\\Software\\openssl-3.0.1\\apps\\client-2048.key'), headers=headers)
- 
-        if resp.status_code == 200:
-          resp_json = resp.json()
-          print(resp_json['loginStatus'])
-          print(resp_json['sessionToken'])
+        if self.resp.status_code == 200:
+          self.resp_json = self.resp.json()
+          print(self.resp_json['loginStatus'])
+          print(self.resp_json['sessionToken'])
+        else:
+          print("Request failed.")
+
+    def logoutTest(self):
+        headers = {'Accept': 'application/json', 'X-Application': 'h3wqckK1N6QPkUrC', 'X-Authentication': self.resp_json['sessionToken']}
+        self.resp2 = requests.post('https://identitysso.betfair.ro/api/logout', headers=headers)
+
+        if self.resp2.status_code == 200:
+          self.resp_json2 = self.resp2.json()
+          print(self.resp_json2['status'])
+          print(self.resp_json2['token'])
         else:
           print("Request failed.")
 
     def APItest(self):
         URL = url = "https://api.betfair.com/exchange/betting/json-rpc/v1"
         jsonrpc_req = '{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listEventTypes", "params": {"filter":{ }}, "id": 1}'
-        headers = {'X-Application': 'h3wqckK1N6QPkUrC', 'X-Authentication': 'nryTHQP6r56YUKlMHEIDwASkdYpYToxQMG8sg7qQlvY=', 'content-type': 'application/json'}
+        headers = {'X-Application': 'h3wqckK1N6QPkUrC', 'X-Authentication': self.resp_json['sessionToken'], 'content-type': 'application/json'}
                                                                             
         try:
             req = urllib.request.Request(url, jsonrpc_req.encode('utf-8'), headers)
             response = urllib.request.urlopen(req)
-            self.jsonResponse = response.read()
-            self.jsonResponse = self.jsonResponse.decode('utf-8')
-            return self.jsonResponse
+            self.eventTypes = response.read()
+        except urllib.error.URLError as e:
+            print (e.reason) 
+            print ('Oops no service available at ' + str(url))
+            exit()
+        except urllib.error.HTTPError:
+            print ('Oops not a valid operation from the service ' + str(url))
+            exit()
+
+    def listEvents(self):
+        URL = url = "https://api.betfair.com/exchange/betting/json-rpc/v1"
+        jsonrpc_req =  '{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listEvents", "params": { "filter": { "eventTypeIds": ["1"], \
+                          "marketStartTime": { "from": "2022-02-17T00:00:00Z", "to": "2022-02-17T23:59:00Z"} } }, "id": 1}'
+        headers = {'X-Application': 'h3wqckK1N6QPkUrC', 'X-Authentication': self.resp_json['sessionToken'], 'content-type': 'application/json'}
+
+        try:
+            req = urllib.request.Request(url, jsonrpc_req.encode('utf-8'), headers)
+            response = urllib.request.urlopen(req)
+            self.events = response.read()
         except urllib.error.URLError as e:
             print (e.reason) 
             print ('Oops no service available at ' + str(url))
@@ -59,4 +87,5 @@ class Main(QMainWindow):
             exit()
 
     def displayResults(self):
-        print(self.jsonResponse)
+        print(self.eventTypes)
+        print(self.events)
